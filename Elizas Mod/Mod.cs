d2e8daa4d73708d0cd772f73d20610e3e29bf4e3
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2022-08-05 17:52:45 +0100

    Updated game files to v2.1.1, added Elizas Mod (Test Mod)

---
+++ Elizas Mod/Mod.cs
@@ -0,0 +1,38 @@
+﻿using MelonLoader;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elizas_Mod
+{
+    public class Mod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            
+        }
+
+        public override void OnUpdate()
+        {
+
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+
+        }
+
+        public override void OnGUI()
+        {
+            if (GUI.Button(new Rect((Screen.width / 2 - 7.5f), 10.0f, 15.0f, 20), "QUACKCORE"))
+            {
+                // PlayerPrefs.GetInt("quackcore") == 1
+                PlayerPrefs.SetInt("quackcore", 1);
+            }
+
+        }
+    }
+}
