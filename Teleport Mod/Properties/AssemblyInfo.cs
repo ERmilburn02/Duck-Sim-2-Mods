7a27c8a545a4b879b875e5d6a2285c349a47d267
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2022-08-27 14:16:02 +0100

    Created Teleport Mod

---
+++ Teleport Mod/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using Teleport_Mod;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Teleport Mod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Teleport Mod")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd051045-6121-4ba3-a361-b6a48562ae17")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: MelonInfo(typeof(TPMod), "Teleport Mod", "0.1.0", "ERmilburn02")]
+[assembly: MelonGame("Orius Games", "Duck Simulator 2")]
---
+++ Teleport Mod/TPMod.cs
@@ -0,0 +1,47 @@
+﻿using MelonLoader;
+using UnityEngine;
+using DS2_Mod_Helper;
+
+namespace Teleport_Mod
+{
+    public class TPMod : MelonMod
+    {
+        private void Teleport(Scenes toScene)
+        {
+            if (SpeedrunManager.instance != null)
+            {
+                if (SpeedrunManager.instance.isRunning)
+                {
+                    SpeedrunManager.instance.isRunning = false;
+                    GameObject.Destroy(SpeedrunManager.instance.gameObject);
+                }
+            }
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            UnityEngine.SceneManagement.SceneManager.LoadScene((int)toScene);
+        }
+
+        public override void OnGUI()
+        {
+            GUILayout.Space(24);
+            GUILayout.Label("Teleport Mod");
+            if (GUILayout.Button("Main Menu (z)")) Teleport(Scenes.MainMenu);
+            if (GUILayout.Button("Clicker (x)")) Teleport(Scenes.ClickerScene);
+            if (GUILayout.Button("Shooter (c)")) Teleport(Scenes.fpsLevel1);
+            if (GUILayout.Button("Platformer (v)")) Teleport(Scenes.platformerLevel1);
+            if (GUILayout.Button("RPG (b)")) Teleport(Scenes.rpgLevel1);
+            if (GUILayout.Button("Credits (n)")) Teleport(Scenes.Credits);
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Z)) Teleport(Scenes.MainMenu);
+            if (Input.GetKeyDown(KeyCode.X)) Teleport(Scenes.ClickerScene);
+            if (Input.GetKeyDown(KeyCode.C)) Teleport(Scenes.fpsLevel1);
+            if (Input.GetKeyDown(KeyCode.V)) Teleport(Scenes.platformerLevel1);
+            if (Input.GetKeyDown(KeyCode.B)) Teleport(Scenes.rpgLevel1);
+            if (Input.GetKeyDown(KeyCode.N)) Teleport(Scenes.Credits);
+        }
+    }
+}
