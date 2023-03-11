6a8754f4f36beb449141dcbf605f119383c78a8f
ERmilburn02 <61363431+ERmilburn02@users.noreply.github.com>
2023-03-11 13:26:15 +0000

    Internal Changes to Mod Helper

--- DS2 Mod Helper/ModHelper.cs
+++ DS2 Mod Helper/ModHelper.cs
@@ -1,5 +1,4 @@
 ﻿using MelonLoader;
-using System.Collections;
 using UnityEngine;
 
 namespace DS2_Mod_Helper
@@ -17,38 +16,5 @@ namespace DS2_Mod_Helper
                 }
             }
         }
-    }
-
-    public enum Scenes : int
-    {
-        MainMenu = 0,
-        ClickerScene,
-        FifthWall,
-        fpsLevel1,
-        fpsLevel2,
-        fpsLevel3,
-        platformerLevel1,
-        platformerLevel2,
-        platformerLevel3,
-        platformerLevel4,
-        platformerLevel5,
-        platformerLevel6,
-        platformerLevel7,
-        rpgLevel1,
-        rpgLevel2,
-        rpgLevel3,
-        rpgLevel4,
-        rpgLevel5,
-        rpgLevel6,
-        rpgLevel7,
-        rpgLevel8,
-        rpgLevel9,
-        rpgLevel10,
-        Credits,
-        ClassicClicker,
-        ClassicShop,
-        ClassicRebirth,
-        ClassicBossFight,
-        TheEnd
     }
 }
---
+++ DS2 Mod Helper/Scenes.cs
@@ -0,0 +1,35 @@
+﻿namespace DS2_Mod_Helper
+{
+    public enum Scenes : int
+    {
+        MainMenu = 0,
+        ClickerScene,
+        FifthWall,
+        fpsLevel1,
+        fpsLevel2,
+        fpsLevel3,
+        platformerLevel1,
+        platformerLevel2,
+        platformerLevel3,
+        platformerLevel4,
+        platformerLevel5,
+        platformerLevel6,
+        platformerLevel7,
+        rpgLevel1,
+        rpgLevel2,
+        rpgLevel3,
+        rpgLevel4,
+        rpgLevel5,
+        rpgLevel6,
+        rpgLevel7,
+        rpgLevel8,
+        rpgLevel9,
+        rpgLevel10,
+        Credits,
+        ClassicClicker,
+        ClassicShop,
+        ClassicRebirth,
+        ClassicBossFight,
+        TheEnd
+    }
+}
